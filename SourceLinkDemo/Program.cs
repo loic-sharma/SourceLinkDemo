500eec21ffbec36ed4fdb80add7e4e8441d1b7c2
Loic Sharma <sharma.loic@gmail.com>
2019-03-15 12:22:48 -0700

    Add demo

--- SourceLinkDemo/Program.cs
+++ SourceLinkDemo/Program.cs
@@ -6,7 +6,55 @@ namespace SourceLinkDemo
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var handler = ChooseHandler();
+
+            while (true)
+            {
+                Console.WriteLine();
+                Console.WriteLine("Please enter two integers:");
+
+                try
+                {
+                    var left = Console.ReadLine();
+                    var right = Console.ReadLine();
+
+                    var result = handler(int.Parse(left), int.Parse(right));
+
+                    Console.WriteLine();
+                    Console.WriteLine($"Result: {result}");
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine();
+                    Console.WriteLine($"Error: {e.Message}");
+                }
+            }
+        }
+
+        public static Func<int, int, int> ChooseHandler()
+        {
+            while (true)
+            {
+                Console.WriteLine("Please select which demo you would like to run:");
+                Console.WriteLine(@"1. ""Bad"" demo");
+                Console.WriteLine(@"2. ""Good"" demo");
+
+                var selection = Console.ReadLine();
+                if (int.TryParse(selection, out var result))
+                {
+                    if (result == 1)
+                    {
+                        return BadDependency.Math.Add;
+                    }
+                    else if (result == 2)
+                    {
+                        return BadDependency.Math.Add;
+                    }
+                }
+
+                Console.WriteLine($"Invalid selection '{selection}', please enter either '1' or '2'");
+                Console.WriteLine();
+            }
         }
     }
 }
--- readme.md
+++ readme.md
@@ -6,6 +6,6 @@
 
 ```ps1
 dotnet pack
-nuget push .\SourceLinkDemo.BadDependency\bin\Debug\SourceLinkDemo.BadDependency.1.0.0.nupkg -Source https://api.nuget.org/v3/index.json
-nuget push .\SourceLinkDemo.GoodDependency\bin\Debug\SourceLinkDemo.GoodDependency.1.0.0.nupkg -Source https://api.nuget.org/v3/index.json
+nuget push .\SourceLinkDemo.BadDependency\bin\Debug\SourceLinkDemo.BadDependency.1.0.1.nupkg -Source https://api.nuget.org/v3/index.json
+nuget push .\SourceLinkDemo.GoodDependency\bin\Debug\SourceLinkDemo.GoodDependency.1.0.1.nupkg -Source https://api.nuget.org/v3/index.json
 ```
